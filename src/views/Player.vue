28e5ef3706322813cc4bb369097509ca7cdc611b
ty888 <1506125048@qq.com>
2016-10-30 11:42:35 +0800

    modify some palyer

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="player">
-    <mt-header :title="playerInfo.fileName">
+    <mt-header :title="playerInfo.fileName" class="header">
       <mt-button icon="back" slot="left">返回</mt-button>
     </mt-header>
-    <div class="player_main" :style="{'background-image': 'url('+singer.image+')'}">
+    <div class="player_main">
+      <div class="mask"></div>
+      <div class="mask_img" :style="{'background-image': 'url('+singer.image+')'}"></div>
       <div class="player_info">
         <div class="player_img">
           <img :src="singer.image">
@@ -28,6 +30,7 @@ export default {
       if (state) {
         if (data.status === 'success') {
           this.playerInfo = data.data
+          new window.Audio(this.playerInfo.url).play()
         }
       } else {
       }
@@ -50,17 +53,43 @@ export default {
 .player{
   height: 100%;
   position: relative;
+  .header{
+    position: fixed;
+    width: 100%;
+    z-index: 10;
+    background-color: #F25D8E;
+  }
   .player_main{
+    padding-top: 40px;
+    position: relative;
     height: 100%;
-    background-position:center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    -webkit-filter: blur(15px);
-    -moz-filter: blur(15px);
-    -o-filter: blur(15px);
-    -ms-filter: blur(15px);
-    filter: blur(15px);
+    .mask{
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #000;
+      opacity: .7;
+      z-index: 9; 
+    }
+    .mask_img{
+      position: absolute;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      -webkit-filter: blur(60px)!important;
+      background-position:center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      -moz-filter: blur(60px);
+      -o-filter: blur(60px);
+      -ms-filter: blur(60px);
+      filter: blur(60px);
+    }
     .player_info{
+      position: relative;
+      z-index: 10;
       padding-top: 50px;
       .player_img{
         height: 180px;
@@ -69,7 +98,7 @@ export default {
         border-radius: 100px;
         border: 30px solid #000;
         margin: 0 auto;
-        animation: img_rotate 4s linear infinite;
+        animation: img_rotate 7s linear infinite;
         img{
           height: 100%;
         }
@@ -86,11 +115,12 @@ export default {
    .control_box{
       padding: 10px;
       height: 70px;
-      background-color: red;
-      position: absolute;
+      background-color: #F25D8E;
+      position: fixed;
       bottom: 0;
       left: 0;
       width: 100%;
+      z-index: 10;
     }
   }
 }
